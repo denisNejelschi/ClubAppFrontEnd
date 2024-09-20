1f8afbd9084730b354f3b226263c1ab53da26e0f
Vitalii Kovtun <kovvv1992@gmail.com>
2024-09-20 18:47:16 +0200

    add proxyConfiguration

--- src/components/loginForm/Login.tsx
+++ src/components/loginForm/Login.tsx
@@ -7,7 +7,9 @@ function LoginForm() {
     <div className={styles.form}>
       <Input name={'email'} type={'email'} placeholder={'Type your email'} label={'Email'} />
       <Input name={'password'} type={'password'} placeholder={'Type your password'} label={'Password'} />
-      <Button />
+      <Button label={''} onClick={function(): void {
+              throw new Error('Function not implemented.');
+          } } />
     </div>
   );
 }
